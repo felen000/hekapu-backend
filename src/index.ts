4f6bd466fda2f02d22612be8f7428d4486b5bba1
Arsen <sapahovarsen@gmail.com>
2025-07-21 18:35:36 +0400

    add middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 import "dotenv/config"
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const PORT = process.env.PORT || 3000;
 const app = express()
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors())
 
 const start = () => {
     try {
