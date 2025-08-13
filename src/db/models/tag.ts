2e9c28b0bd90d67a456de20eb903624e0c99270d
Arsen <sapahovarsen@gmail.com>
2025-08-13 21:21:23 +0400

    add creation attributes to tag model

--- src/db/models/tag.ts
+++ src/db/models/tag.ts
@@ -2,8 +2,12 @@ import {Table, Column, Model, HasMany, Unique, BelongsToMany, AllowNull} from "s
 import {Post} from "./post.model.js";
 import {PostTag} from "./post-tag.model.js";
 
+interface TagCreationAttrs {
+    name: string;
+}
+
 @Table
-export class Tag extends Model<Tag> {
+export class Tag extends Model<Tag, TagCreationAttrs> {
     @Unique
     @AllowNull(false)
     @Column
