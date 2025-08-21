1ddd7be09d46b359d95a5c1e82a97cd0aa570577
Arsen <sapahovarsen@gmail.com>
2025-08-21 18:56:07 +0400

    fix profile picture directory

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,6 +5,6 @@ const __filename = fileURLToPath(import.meta.url);
 export const __dirname = path.join(path.dirname(__filename), '../') //root directory
 export const PUBLIC_DIRECTORY = path.join(__dirname, '../', '/public');
 export const POST_PICTURE_DIRECTORY = path.join(PUBLIC_DIRECTORY, '/posts');
-export const PROFILE_PICTURE_DIRECTORY = path.join(PUBLIC_DIRECTORY, '/posts');
+export const PROFILE_PICTURE_DIRECTORY = path.join(PUBLIC_DIRECTORY, '/users');
 export const REFRESH_COOKIE_OPTIONS = {maxAge: 15 * 24 * 60 * 60 * 1000, httpOnly: true};
 
