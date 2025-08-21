1e45d2b38d1f0c64dfb885f036ac340544ee838b
Arsen <sapahovarsen@gmail.com>
2025-08-21 19:04:02 +0400

    add service for image processing

---
+++ src/services/image.service.ts
@@ -0,0 +1,41 @@
+import sharp from "sharp";
+import {POST_PICTURE_DIRECTORY, PROFILE_PICTURE_DIRECTORY} from "../constants/index.js";
+import {UploadedFile} from "express-fileupload";
+import {v4} from "uuid";
+import path from "path";
+import {ApiError} from "../exceptions/api-error.js";
+import * as fs from "node:fs";
+
+class ImageService {
+    async savePostImage(image: UploadedFile): Promise<string> {
+        try {
+            if (!fs.existsSync(POST_PICTURE_DIRECTORY)) {
+                fs.mkdirSync(POST_PICTURE_DIRECTORY, {recursive: true});
+            }
+            const fileName = v4() + '.jpg';
+            const filePath = path.join(POST_PICTURE_DIRECTORY, fileName);
+            await sharp(image.data).jpeg().toFile(filePath);
+            return process.env.API_URL + '/public/posts/' + fileName;
+        } catch (e) {
+            console.log('Ошибка при обработке изображения:', e);
+            throw ApiError.InternalServerError('Ошибка при обработке изображения.');
+        }
+    }
+
+    async saveProfilePicture(image: UploadedFile): Promise<string> {
+        try {
+            if (!fs.existsSync(PROFILE_PICTURE_DIRECTORY)) {
+                fs.mkdirSync(PROFILE_PICTURE_DIRECTORY, {recursive: true});
+            }
+            const fileName = v4() + '.jpg';
+            const filePath = path.join(PROFILE_PICTURE_DIRECTORY, fileName);
+            await sharp(image.data).jpeg().toFile(filePath);
+            return process.env.API_URL + '/public/users/' + fileName;
+        } catch (e) {
+            console.log('Ошибка при обработке изображения:', e);
+            throw ApiError.InternalServerError('Ошибка при обработке изображения.');
+        }
+    }
+}
+
+export default new ImageService();
